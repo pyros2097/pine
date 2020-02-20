395707e9c97c9cbe7ecad07382c7b0477686a20a
pyros2097 <pyros2097@gmail.com>
2020-02-20 23:09:18 +0530

    working for 2 funcs

--- ast/.snapshots/main.go
+++ ast/.snapshots/main.go
@@ -10,36 +10,80 @@
       Funs: []*ast.FunDecl{
         &ast.FunDecl{
           Pos: Position{Filename: "", Offset: 14, Line: 3, Column: 1},
+          Name: "sub",
+          Parameters: []*ast.MethodParameter{
+            &ast.MethodParameter{
+              Pos: Position{Filename: "", Offset: 23, Line: 3, Column: 10},
+              ID: "a",
+              Type: &ast.Type{
+                Pos: Position{Filename: "", Offset: 24, Line: 3, Column: 11},
+                Name: "float",
+              },
+            },
+            &ast.MethodParameter{
+              Pos: Position{Filename: "", Offset: 33, Line: 3, Column: 20},
+              ID: "b",
+              Type: &ast.Type{
+                Pos: Position{Filename: "", Offset: 34, Line: 3, Column: 21},
+                Name: "float",
+              },
+            },
+          },
+          ReturnType: &ast.ReturnType{
+            Pos: Position{Filename: "", Offset: 43, Line: 3, Column: 30},
+            Name: "float",
+          },
+          Body: []*ast.Statement{
+            &ast.Statement{
+              Pos: Position{Filename: "", Offset: 54, Line: 4, Column: 3},
+              ReturnStatement: &ast.ReturnStatement{
+                Pos: Position{Filename: "", Offset: 54, Line: 4, Column: 3},
+                ReturnLiteral: &ast.ReturnLiteral{
+                  Pos: Position{Filename: "", Offset: 61, Line: 4, Column: 10},
+                  Literal: a,
+                },
+              },
+            },
+          },
+          End: Position{Filename: "", Offset: 0, Line: 0, Column: 0},
+        },
+      },
+    },
+    &ast.Entry{
+      Pos: Position{Filename: "", Offset: 70, Line: 8, Column: 1},
+      Funs: []*ast.FunDecl{
+        &ast.FunDecl{
+          Pos: Position{Filename: "", Offset: 70, Line: 8, Column: 1},
           Name: "main",
           Parameters: []*ast.MethodParameter{
             &ast.MethodParameter{
-              Pos: Position{Filename: "", Offset: 24, Line: 3, Column: 11},
+              Pos: Position{Filename: "", Offset: 80, Line: 8, Column: 11},
               ID: "a",
               Type: &ast.Type{
-                Pos: Position{Filename: "", Offset: 25, Line: 3, Column: 12},
+                Pos: Position{Filename: "", Offset: 81, Line: 8, Column: 12},
                 Name: "int",
               },
             },
             &ast.MethodParameter{
-              Pos: Position{Filename: "", Offset: 32, Line: 3, Column: 19},
+              Pos: Position{Filename: "", Offset: 88, Line: 8, Column: 19},
               ID: "b",
               Type: &ast.Type{
-                Pos: Position{Filename: "", Offset: 33, Line: 3, Column: 20},
+                Pos: Position{Filename: "", Offset: 89, Line: 8, Column: 20},
                 Name: "int",
               },
             },
           },
           ReturnType: &ast.ReturnType{
-            Pos: Position{Filename: "", Offset: 40, Line: 3, Column: 27},
+            Pos: Position{Filename: "", Offset: 96, Line: 8, Column: 27},
             Name: "int",
           },
           Body: []*ast.Statement{
             &ast.Statement{
-              Pos: Position{Filename: "", Offset: 49, Line: 4, Column: 3},
+              Pos: Position{Filename: "", Offset: 105, Line: 9, Column: 3},
               ReturnStatement: &ast.ReturnStatement{
-                Pos: Position{Filename: "", Offset: 49, Line: 4, Column: 3},
+                Pos: Position{Filename: "", Offset: 105, Line: 9, Column: 3},
                 ReturnLiteral: &ast.ReturnLiteral{
-                  Pos: Position{Filename: "", Offset: 56, Line: 4, Column: 10},
+                  Pos: Position{Filename: "", Offset: 112, Line: 9, Column: 10},
                   Literal: a,
                 },
               },
--- code_gen/code.go
+++ code_gen/code.go
@@ -7,6 +7,16 @@ import (
 
 var typeMap = map[string]byte{
 	"int": 0x7f,
+	"float": 0x7c,
+}
+
+var operandMap = map[string]map[string]byte{
+	"int": map[string]byte{
+		"add": 0x6a,
+	},
+	"float": map[string]byte{
+		"add": 0xa0,
+	},
 }
 
 func GenerateFunc(fun *ast.FunDecl) {
@@ -28,7 +38,7 @@ func GenerateCode(w io.Writer, ast *ast.Ast) {
 				types = append(types, typeMap[v.Type.Name])
 			}
 			types = append(types, 0x01)                         // num results
-			types = append(types, typeMap[fun.ReturnType.Name]) //i32
+			types = append(types, typeMap[fun.ReturnType.Name]) // i32
 		}
 		for _, fun := range entry.Funs {
 			funcs = append(funcs, byte(funcCount)) // function 0 signature index
@@ -41,7 +51,7 @@ func GenerateCode(w io.Writer, ast *ast.Ast) {
 				0x00, // local decl count
 				0x20, 0x00, //  local.get, local index
 				0x20, 0x01, //  local.get, local index
-				0x6a, 0x0b, //  i32.add, end
+				operandMap[fun.ReturnType.Name]["add"], 0x0b, //  i32.add, end
 			}
 			funcBodys = append(funcBodys, byte(len(funcbody))) // func body size
 			funcBodys = append(funcBodys, funcbody...)
@@ -52,10 +62,8 @@ func GenerateCode(w io.Writer, ast *ast.Ast) {
 	w.Write(types)
 	w.Write([]byte{0x03, byte(len(funcs) + 1), byte(funcCount)}) // Func sig section code, section size, num types, type data
 	w.Write(funcs)
-
 	w.Write([]byte{0x07, byte(len(exports) + 1), byte(funcCount)}) // exports section code, section size, num exports
 	w.Write(exports)
-
 	w.Write([]byte{0x0a, byte(len(funcBodys) + 1), byte(funcCount)}) // func body section code, section size, num functions
 	w.Write(funcBodys)
 }
@@ -75,75 +83,3 @@ func GenerateCode(w io.Writer, ast *ast.Ast) {
 // block (result i32 i32)
 //   ...
 // end
-
-// 0000000: 0061 736d                                 ; WASM_BINARY_MAGIC
-// 0000004: 0100 0000                                 ; WASM_BINARY_VERSION
-// ; section "Type" (1)
-// 0000008: 01                                        ; section code
-// 0000009: 00                                        ; section size (guess)
-// 000000a: 02                                        ; num types
-// ; type 0
-// 000000b: 60                                        ; func
-// 000000c: 02                                        ; num params
-// 000000d: 7c                                        ; f64
-// 000000e: 7c                                        ; f64
-// 000000f: 01                                        ; num results
-// 0000010: 7c                                        ; f64
-// ; type 1
-// 0000011: 60                                        ; func
-// 0000012: 02                                        ; num params
-// 0000013: 7f                                        ; i32
-// 0000014: 7f                                        ; i32
-// 0000015: 01                                        ; num results
-// 0000016: 7f                                        ; i32
-// 0000009: 0d                                        ; FIXUP section size
-// ; section "Function" (3)
-// 0000017: 03                                        ; section code
-// 0000018: 00                                        ; section size (guess)
-// 0000019: 02                                        ; num functions
-// 000001a: 00                                        ; function 0 signature index
-// 000001b: 01                                        ; function 1 signature index
-// 0000018: 03                                        ; FIXUP section size
-// ; section "Export" (7)
-// 000001c: 07                                        ; section code
-// 000001d: 00                                        ; section size (guess)
-// 000001e: 02                                        ; num exports
-// 000001f: 03                                        ; string length
-// 0000020: 7375 62                                  sub  ; export name
-// 0000023: 00                                        ; export kind
-// 0000024: 00                                        ; export func index
-// 0000025: 04                                        ; string length
-// 0000026: 6d61 696e                                main  ; export name
-// 000002a: 00                                        ; export kind
-// 000002b: 01                                        ; export func index
-// 000001d: 0e                                        ; FIXUP section size
-// ; section "Code" (10)
-// 000002c: 0a                                        ; section code
-// 000002d: 00                                        ; section size (guess)
-// 000002e: 02                                        ; num functions
-// ; function body 0
-// 000002f: 00                                        ; func body size (guess)
-// 0000030: 00                                        ; local decl count
-// 0000031: 20                                        ; local.get
-// 0000032: 00                                        ; local index
-// 0000033: 20                                        ; local.get
-// 0000034: 01                                        ; local index
-// 0000035: a0                                        ; f64.add
-// 0000036: 0b                                        ; end
-// 000002f: 07                                        ; FIXUP func body size
-// ; function body 1
-// 0000037: 00                                        ; func body size (guess)
-// 0000038: 00                                        ; local decl count
-// 0000039: 20                                        ; local.get
-// 000003a: 00                                        ; local index
-// 000003b: 20                                        ; local.get
-// 000003c: 01                                        ; local index
-// 000003d: 6a                                        ; i32.add
-// 000003e: 0b                                        ; end
-// 0000037: 07                                        ; FIXUP func body size
-// 000002d: 11                                        ; FIXUP section size
-// ;; dump
-// 0000000: 0061 736d 0100 0000 010d 0260 027c 7c01
-// 0000010: 7c60 027f 7f01 7f03 0302 0001 070e 0203
-// 0000020: 7375 6200 0004 6d61 696e 0001 0a11 0207
-// 0000030: 0020 0020 01a0 0b07 0020 0020 016a 0b
